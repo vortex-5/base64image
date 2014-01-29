7be699f82bd8fb9a84f7bcc29f35f5750cdc8e79
nmmf <stefan.paulin@all-inkl.com>
2014-01-29 11:18:03 +0100

    Create de.js

---
+++ lang/de.js
@@ -0,0 +1,25 @@
+/*
+ * Created by ALL-INKL.COM - Neue Medien Münnich - 29. Jan 2014
+ * Licensed under the terms of GPL, LGPL and MPL licenses.
+ */
+CKEDITOR.plugins.setLang("base64image", "de", {
+	title : "Bild",
+	tabsource : "Bildquelle",
+	tabprop : "Eigenschaften",
+	url : "URL",
+	file : "Datei",
+	loading : "Lade...",
+	alt : "Alternativer Text",
+	width : "Breite",
+	height : "Höhe",
+	lock : "Proportionen beibehalten",
+	vmargin : "Vertikaler Abstand",
+	hmargin : "Horizontaler Abstand",
+	border : "Rand",
+	align : "Ausrichtung",
+	alignnone : "Ohne",
+	aligntop : "Oben",
+	alignbottom : "Unten",
+	alignleft : "Links",
+	alignright : "Rechts"
+});
